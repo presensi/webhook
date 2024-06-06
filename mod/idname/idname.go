1775874308273ec0d5be2daf3266779f677530d6
fahiraaa06 <fahira1678@gmail.com>
2024-06-06 12:16:11 +0700

    added: post data

--- mod/idname/idname.go
+++ mod/idname/idname.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/gocroot/helper/atdb"
 	"github.com/whatsauth/itmodel"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -37,14 +37,10 @@ func GetLokasi(mongoconn *mongo.Database, long float64, lat float64) (namalokasi
 	return lokasi.Nama
 }
 
-type Lokasi struct { //lokasi yang bisa melakukan presensi
-	ID       primitive.ObjectID `bson:"_id,omitempty"`
-	Nama     string             `bson:"nama,omitempty"`
-	Batas    Geometry           `bson:"batas,omitempty"`
-	Kategori string             `bson:"kategori,omitempty"`
-}
-
-type Geometry struct { //data geometry untuk lokasi presensi
-	Type        string      `json:"type" bson:"type"`
-	Coordinates interface{} `json:"coordinates" bson:"coordinates"`
+func Postdatapresensi(mongoconn *mongo.Database, long float64, lat float64) {
+	lokasi := GetLokasi(mongoconn, long, lat)
+	data := DataPresensi{
+		Lokasi: lokasi,
+	}
+	atdb.InsertOneDoc(mongoconn, "presensi", data)
 }
