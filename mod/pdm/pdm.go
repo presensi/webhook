3f4f2a64050b384705b76c66b38423b9eeba9787
haryadi <haryadiyusuf61@gmail.com>
2024-06-13 11:15:01 +0700

    perbaiki pdm

--- mod/pdm/pdm.go
+++ mod/pdm/pdm.go
@@ -5,10 +5,16 @@ import "github.com/whatsauth/itmodel"
 // Pdm generates a reply string based on the provided IteungMessage.
 func Pdm(Pesan itmodel.IteungMessage) (reply string) {
 	return `Nomor Induk Mahasiswa: Kartu Tanda Mahasiswa, Ijazah dan Transkrip Nilai (Apabila Mahasiswa sudah lulus), Kartu Hasil Studi
+
 Nama Mahasiswa: Akte Kelahiran atau Surat Kenal Lahir atau Kartu Keluarga atau Ijazah, Kartu Tanda Mahasiswa, Ijazah dan Transkrip Nilai (Apabila Mahasiswa sudah lulus)
+
 Nama Ibu Kandung: Akte Kelahiran atau Surat Kenal Lahir atau Kartu Keluarga
+
 Tempat Lahir: Akte Kelahiran atau Surat Kenal Lahir atau Kartu Keluarga atau Ijazah, Kartu Tanda Mahasiswa, Ijazah dan Transkrip Nilai (Apabila Mahasiswa sudah lulus)
+
 Tanggal Lahir: Akte Kelahiran atau Surat Kenal Lahir atau Kartu Keluarga atau Ijazah, Kartu Tanda Mahasiswa, Ijazah dan Transkrip Nilai (Apabila Mahasiswa sudah lulus)
+
 Periode Pendaftaran: Surat Penerimaan Mahasiswa
+
 Jenis Kelamin: Mengikuti Persyaratan Umum`
 }
