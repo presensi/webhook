50bcd02c17ff413150222382d04864cbf2afcb93
Sam9100 <mhaitsamia@gmail.com>
2024-06-25 22:24:00 +0700

    tempo

--- mod/idname/idname.go
+++ mod/idname/idname.go
@@ -1,6 +1,7 @@
 package idname
 
 import (
+	"context"
 	"fmt"
 	// "net/http"
 	// "strconv"
@@ -13,6 +14,16 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+// func IDName(Pesan itmodel.IteungMessage, db *mongo.Database) (reply string) {
+// 	if !Pesan.LiveLoc {
+// 		return "Minimal share live location dulu lah kak."
+// 	}
+// 	longitude := fmt.Sprintf("%f", Pesan.Longitude)
+// 	latitude := fmt.Sprintf("%f", Pesan.Latitude)
+
+// 	return "Hai.. hai.. kakak atas nama:\n" + Pesan.Alias_name + "\nLongitude: " + longitude + "\nLatitude: " + latitude + "\nLokasi:" + GetLokasi(db, Pesan.Longitude, Pesan.Latitude) + "\nTelah berhasil melakukan absen\nTerimakasih"
+// }
+
 func IDNameMasuk(Pesan itmodel.IteungMessage, db *mongo.Database) (reply string) {
 	if !Pesan.LiveLoc {
 		return "Minimal share live location dulu lah kak " + Pesan.Alias_name
@@ -21,10 +32,10 @@ func IDNameMasuk(Pesan itmodel.IteungMessage, db *mongo.Database) (reply string)
 	latitude := fmt.Sprintf("%f", Pesan.Latitude)
 	lokasiuser, err := GetLokasi(db, Pesan.Longitude, Pesan.Latitude)
 	if err != nil {
-		return "Mohon maaf kak, kakak " + Pesan.Alias_name + " belum berada di lokasi presensi, silahkan menuju lokasi presensi dahulu baru bisa presensi masuk kak."
+		return "Mohon maaf kak, kakak " + Pesan.Alias_name + " belum berada di lokasi presensi, silahkan menuju lokasi presensi dahulu baru presensi masuk."
 	}
 	if lokasiuser.Nama == "" {
-		return "Nama nya kok kosong kak?! " + Pesan.Alias_name
+		return "Nama nya kosong kak " + Pesan.Alias_name
 	}
 	dtuser := &PresensiLokasi{
 		PhoneNumber: Pesan.Phone_number,
@@ -34,45 +45,58 @@ func IDNameMasuk(Pesan itmodel.IteungMessage, db *mongo.Database) (reply string)
 	}
 	_, err = atdb.InsertOneDoc(db, "presensi", dtuser)
 	if err != nil {
-		return "Gagal insert ke database nih kak :( " + Pesan.Alias_name
+		return "Gagal insert ke database kak " + Pesan.Alias_name
 	}
 
-	return "Hai.. hai.. kakak atas nama:\n" + Pesan.Alias_name + "\nLongitude: " + longitude + "\nLatitude: " + latitude + "\nLokasi: " + lokasiuser.Nama + "\nTelah berhasil melakukan absensi masuk\nTerimakasih"
+	return "Hai.. hai.. kakak atas nama:\n" + Pesan.Alias_name + "\nLongitude: " + longitude + "\nLatitude: " + latitude + "\nLokasi:" + lokasiuser.Nama + "\nTelah berhasil melakukan absensi masuk\nTerimakasih"
 }
 
 func IDNamePulang(Pesan itmodel.IteungMessage, db *mongo.Database) (reply string) {
 	if !Pesan.LiveLoc {
 		return "Minimal share live location dulu lah kak " + Pesan.Alias_name
 	}
-	longitude := fmt.Sprintf("%f", Pesan.Longitude) 	
+	longitude := fmt.Sprintf("%f", Pesan.Longitude)
 	latitude := fmt.Sprintf("%f", Pesan.Latitude)
 	lokasiuser, err := GetLokasi(db, Pesan.Longitude, Pesan.Latitude)
 	if err != nil {
-		return "Mohon maaf kak " + Pesan.Alias_name + ", kakak belum berada di lokasi presensi, silahkan menuju lokasi presensi dahulu baru bisa presensi pulang."
+		return "Mohon maaf kak, kakak " + Pesan.Alias_name + " belum berada di lokasi presensi, silahkan menuju lokasi presensi dahulu baru presensi masuk."
 	}
 	if lokasiuser.Nama == "" {
-		return "Nama nya kok kosong kak?! " + Pesan.Alias_name
+		return "Nama nya kosong kak " + Pesan.Alias_name
 	}
 	dtuser := &PresensiLokasi{
 		PhoneNumber: Pesan.Phone_number,
 		Lokasi:      lokasiuser,
-		IsMasuk:     false,
+		IsMasuk:     true,
 		CreatedAt:   time.Now(),
-	}
-	filter := bson.M{"_id": atdb.TodayFilter(), "cekinlokasi.phonenumber": Pesan.Phone_number, "ismasuk": true}
-	docselfie, err := atdb.GetOneLatestDoc[PresensiSelfie](db, "selfie", filter)
-	if err != nil {
-		return "Kakak " + Pesan.Alias_name + " belum selfie masuk ini :( " + err.Error()
-	}
-	if docselfie.CekInLokasi.Lokasi.ID != lokasiuser.ID {
-		return "Lokasi pulang nya harus sama dengan lokasi masuknya dong kak " + Pesan.Alias_name + ".\nLokasi : " + lokasiuser.Nama
 	}
 	_, err = atdb.InsertOneDoc(db, "presensi", dtuser)
 	if err != nil {
-		return "Gagal insert ke database nih kak :( " + Pesan.Alias_name
+		return "Gagal insert ke database kak " + Pesan.Alias_name
 	}
 
-	return "Hai.. hai.. kakak atas nama:\n" + Pesan.Alias_name + "\nLongitude: " + longitude + "\nLatitude: " + latitude + "\nLokasi: " + lokasiuser.Nama + "\nTelah berhasil melakukan absensi keluar\nTerimakasih"
+	return "Hai.. hai.. kakak atas nama:\n" + Pesan.Alias_name + "\nLongitude: " + longitude + "\nLatitude: " + latitude + "\nLokasi:" + lokasiuser.Nama + "\nTelah berhasil melakukan absensi masuk\nTerimakasih"
+}
+
+func GetLokasi(mongoconn *mongo.Database, long float64, lat float64) (Lokasi, error) {
+	lokasicollection := mongoconn.Collection("lokasi")
+	filter := bson.M{
+		"batas": bson.M{
+			"$geoIntersects": bson.M{
+				"$geometry": bson.M{
+					"type":        "Point",
+					"coordinates": []float64{long, lat},
+				},
+			},
+		},
+	}
+	var lokasi Lokasi
+	err := lokasicollection.FindOne(context.TODO(), filter).Decode(&lokasi)
+	if err != nil {
+		fmt.Printf("GetLokasi: %v\n", err)
+		return Lokasi{}, err
+	}
+	return lokasi, nil
 }
 
 // func CekSelfieMasuk(Profile itmodel.Profile, Pesan itmodel.IteungMessage, db *mongo.Database) (reply string) {
@@ -205,21 +229,21 @@ func IDNamePulang(Pesan itmodel.IteungMessage, db *mongo.Database) (reply string
 
 // }
 
-func GetLokasi(mongoconn *mongo.Database, long float64, lat float64) (lokasi Lokasi, err error) {
-	filter := bson.M{
-		"batas": bson.M{
-			"$geoIntersects": bson.M{
-				"$geometry": bson.M{
-					"type":        "Point",
-					"coordinates": []float64{long, lat},
-				},
-			},
-		},
-	}
+// func GetLokasi(mongoconn *mongo.Database, long float64, lat float64) (lokasi Lokasi, err error) {
+// 	filter := bson.M{
+// 		"batas": bson.M{
+// 			"$geoIntersects": bson.M{
+// 				"$geometry": bson.M{
+// 					"type":        "Point",
+// 					"coordinates": []float64{long, lat},
+// 				},
+// 			},
+// 		},
+// 	}
 
-	lokasi, err = atdb.GetOneDoc[Lokasi](mongoconn, "lokasi", filter)
-	if err != nil {
-		return
-	}
-	return
-}
+// 	lokasi, err = atdb.GetOneDoc[Lokasi](mongoconn, "lokasi", filter)
+// 	if err != nil {
+// 		return
+// 	}
+// 	return
+// }
