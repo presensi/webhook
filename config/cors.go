2f885fd33c639d90f30143743532d8905bc4830f
Sam9100 <mhaitsamia@gmail.com>
2024-06-26 15:07:21 +0700

    tes cors 3

--- config/cors.go
+++ config/cors.go
@@ -40,7 +40,7 @@ func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
 			w.Header().Set("Access-Control-Allow-Methods", "POST,GET,OPTIONS")
-			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Origin", "*")
 			w.Header().Set("Access-Control-Max-Age", "3600")
 			w.WriteHeader(http.StatusNoContent)
 			return true
