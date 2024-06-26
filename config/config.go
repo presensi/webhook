2e42643d86275e9a24be2ebbea90019dd496422c
Sam9100 <mhaitsamia@gmail.com>
2024-06-26 13:29:29 +0700

    new func

--- config/config.go
+++ config/config.go
@@ -1,7 +1,21 @@
 package config
 
 import (
+	"log"
+
 	"github.com/gocroot/helper"
 )
 
 var IPPort, Net = helper.GetAddress()
+
+func SetEnv() {
+	if ErrorMongoconn != nil {
+		log.Println(ErrorMongoconn.Error())
+	}
+	// 	profile, err := atdb.GetOneDoc[model.Profile](Mongoconn, "profile", primitive.M{})
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 	}
+	// 	PublicKeyWhatsAuth = profile.PublicKey
+	// 	WAAPIToken = profile.Token
+}
