eadc79c08921c3ca641729fb74447ad5a7820e00
Sam9100 <mhaitsamia@gmail.com>
2024-06-27 12:38:00 +0700

    new model

---
+++ model/model.go
@@ -0,0 +1,10 @@
+package model
+
+type Profile struct {
+	Token       string `bson:"token"`
+	Phonenumber string `bson:"phonenumber"`
+	Secret      string `bson:"secret"`
+	URL         string `bson:"url"`
+	QRKeyword   string `bson:"qrkeyword"`
+	PublicKey   string `bson:"publickey"`
+}
