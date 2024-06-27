257fadedfc2bf2981aa0124c37d77112ea703384
Sam9100 <mhaitsamia@gmail.com>
2024-06-27 11:52:43 +0700

    add helper

--- helper/atdb/mongo.go
+++ helper/atdb/mongo.go
@@ -2,12 +2,14 @@ package atdb
 
 import (
 	"context"
+	"fmt"
 	"net"
 	"strconv"
 	"strings"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -60,6 +62,35 @@ func SRVLookup(srvuri string) (mongouri string) {
 	return
 }
 
+func GetAllDistinctDoc(db *mongo.Database, filter bson.M, fieldname, collection string) (doc []any, err error) {
+	ctx := context.TODO()
+	doc, err = db.Collection(collection).Distinct(ctx, fieldname, filter)
+	if err != nil {
+		return nil, err
+	}
+	return doc, nil
+}
+
+// GetAllDistinctDoc mengambil semua nilai yang berbeda dari field tertentu dalam koleksi yang diberikan
+func GetAllDistinct[T any](db *mongo.Database, filter bson.M, fieldname, collection string) ([]T, error) {
+	ctx := context.TODO()
+	rawDoc, err := db.Collection(collection).Distinct(ctx, fieldname, filter)
+	if err != nil {
+		return nil, err
+	}
+
+	// Mengkonversi []interface{} ke []T
+	result := make([]T, len(rawDoc))
+	for i, v := range rawDoc {
+		value, ok := v.(T)
+		if !ok {
+			return nil, fmt.Errorf("type assertion to %T failed", v)
+		}
+		result[i] = value
+	}
+	return result, nil
+}
+
 func GetRandomDoc[T any](db *mongo.Database, collection string, size uint) (result []T, err error) {
 	filter := mongo.Pipeline{
 		{{Key: "$sample", Value: bson.D{{Key: "size", Value: size}}}},
@@ -97,6 +128,16 @@ func GetOneDoc[T any](db *mongo.Database, collection string, filter bson.M) (doc
 	return
 }
 
+func DeleteManyDocs(db *mongo.Database, collection string, filter bson.M) (deleteresult *mongo.DeleteResult, err error) {
+	deleteresult, err = db.Collection(collection).DeleteMany(context.Background(), filter)
+	return
+}
+
+func DeleteOneDoc(db *mongo.Database, collection string, filter bson.M) (updateresult *mongo.DeleteResult, err error) {
+	updateresult, err = db.Collection(collection).DeleteOne(context.Background(), filter)
+	return
+}
+
 func GetOneLatestDoc[T any](db *mongo.Database, collection string, filter bson.M) (doc T, err error) {
 	opts := options.FindOne().SetSort(bson.M{"$natural": -1})
 	err = db.Collection(collection).FindOne(context.TODO(), filter, opts).Decode(&doc)
@@ -106,34 +147,50 @@ func GetOneLatestDoc[T any](db *mongo.Database, collection string, filter bson.M
 	return
 }
 
-func InsertOneDoc(db *mongo.Database, collection string, doc interface{}) (insertedID interface{}, err error) {
+func InsertOneDoc(db *mongo.Database, collection string, doc interface{}) (insertedID primitive.ObjectID, err error) {
 	insertResult, err := db.Collection(collection).InsertOne(context.TODO(), doc)
 	if err != nil {
 		return
 	}
-	return insertResult.InsertedID, nil
+	return insertResult.InsertedID.(primitive.ObjectID), nil
 }
 
-// With replaceOne() you can only replace the entire document,
-// while updateOne() allows for updating fields. Since replaceOne() replaces the entire document - fields in the old document not contained in the new will be lost.
-// With updateOne() new fields can be added without losing the fields in the old document.
-func UpdateDoc(db *mongo.Database, collection string, filter bson.M, updatefield bson.M) (updateresult *mongo.UpdateResult, err error) {
-	updateresult, err = db.Collection(collection).UpdateOne(context.TODO(), filter, updatefield)
+// Fungsi untuk menyisipkan banyak dokumen ke dalam koleksi: insertedIDs, err := InsertManyDocs(db, collection, docs)
+func InsertManyDocs[T any](db *mongo.Database, collection string, docs []T) (insertedIDs []interface{}, err error) {
+	// Konversi []T ke []interface{}
+	interfaceDocs := make([]interface{}, len(docs))
+	for i, v := range docs {
+		interfaceDocs[i] = v
+	}
+
+	insertResult, err := db.Collection(collection).InsertMany(context.TODO(), interfaceDocs)
+	if err != nil {
+		return nil, err
+	}
+	return insertResult.InsertedIDs, nil
+}
+
+// With UpdateOneDoc() allows for updating fields, new fields can be added without losing the fields in the old document.
+//
+//	updatefields := bson.M{
+//		"token":         token.AccessToken,
+//		"refresh_token": token.RefreshToken,
+//		"expiry":        token.Expiry,
+//	}
+func UpdateOneDoc(db *mongo.Database, collection string, filter bson.M, updatefields bson.M) (updateresult *mongo.UpdateResult, err error) {
+	updateresult, err = db.Collection(collection).UpdateOne(context.TODO(), filter, bson.M{"$set": updatefields}, options.Update().SetUpsert(true))
 	if err != nil {
 		return
 	}
 	return
 }
 
+// With ReplaceOneDoc() you can only replace the entire document,
+// while UpdateOneDoc() allows for updating fields. Since ReplaceOneDoc() replaces the entire document - fields in the old document not contained in the new will be lost.
 func ReplaceOneDoc(db *mongo.Database, collection string, filter bson.M, doc interface{}) (updatereseult *mongo.UpdateResult, err error) {
 	updatereseult, err = db.Collection(collection).ReplaceOne(context.TODO(), filter, doc)
 	if err != nil {
 		return
 	}
 	return
-}
-
-func DeleteOneDoc(db *mongo.Database, collection string, filter bson.M) error {
-	_, err := db.Collection(collection).DeleteOne(context.Background(), filter)
-	return err
 }
