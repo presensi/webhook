fe57cbcf49abfc9fcd9236963c4d75bb2aab9898
haryadi <haryadiyusuf61@gmail.com>
2024-07-08 23:05:39 +0700

    ganti url

--- config/api.go
+++ config/api.go
@@ -14,4 +14,4 @@ var PublicKeyWhatsAuth string
 
 var WAAPIToken string
 
-var DOKUMENPANDUAN, ErrDokumen = file.DownloadFileBase64("https://github.com/haryadi14/jurnal_peningkatan-/blob/master/panduan.pdf")
+var DOKUMENPANDUAN, ErrDokumen = file.DownloadFileBase64("https://raw.githubusercontent.com/haryadi14/jurnal_peningkatan-/master/panduan.pdf")
