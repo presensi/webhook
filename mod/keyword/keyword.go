c0efb3403aec1e4139a07751abf73a43d38088eb
haryadi <haryadiyusuf61@gmail.com>
2024-07-11 17:46:13 +0700

    fixing

--- mod/keyword/keyword.go
+++ mod/keyword/keyword.go
@@ -8,10 +8,10 @@ import (
 
 func Keyword(Pesan itmodel.IteungMessage) (reply string) {
 	return fmt.Sprintf("Hai.. hai.. %s\n\nBerikut adalah Keyword yang tersedia:\n\n"+
-		"1. *minta persyaratan pdm\n"+
-		"2. *minta persyaratan pdmk\n"+
-		"3. *minta persyaratan validasi ijazah\n"+
-		"4. *minta persyaratan nidn\n"+
-		"5. *minta dokumen panduan\n",
+		"1. *minta persyaratan pdm (Adalah keywords untuk meminta persyaratan perubahan data mahasiswa)\n"+
+		"2. *minta persyaratan pdmk (Adalah keywords untuk meminta persyaratan perubahan data mahasiswa jenis keluar)\n"+
+		"3. *minta persyaratan validasi ijazah (Adalah keywords untuk meminta persyaratan untuk memvalidasi ijazah mahasiswa)\n"+
+		"4. *minta persyaratan nidn (Adalah keywords untuk meminta persyaratan Regristasi Dosen Baru NIDN)\n"+
+		"5. *minta dokumen panduan (Adalah keywords untuk meminta dokumen panduan)\n",
 		Pesan.Alias_name)
 }
