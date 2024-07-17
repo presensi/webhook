c590d0bce2b3d99f66e0f4bfbea184fa274c1d3a
Sam9100 <mhaitsamia@gmail.com>
2024-07-18 01:16:25 +0700

    add struct

--- model/sistem.go
+++ model/sistem.go
@@ -1,6 +1,13 @@
 package model
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	// "time"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/bson"
+	// "go.mongodb.org/mongo-driver/bson/primitive"
+	"context"
+)
 
 // Untuk Login
 type AdminRegistration struct {
@@ -23,4 +30,46 @@ type Siswa struct {
     Nama  string             `bson:"nama" json:"nama"`
     Kelas string             `bson:"kelas" json:"kelas"`
     Umur  int                `bson:"umur" json:"umur"`
+}
+
+// Untuk Kehadrian
+type Kehadiran struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	Date      string             `bson:"date"`
+	Name      string             `bson:"name"`
+	Subject   string             `bson:"subject"`
+	Status    string             `bson:"status"`
+}
+
+func AddKehadiran(db *mongo.Database, kehadiran Kehadiran) error {
+	_, err := db.Collection("kehadiran").InsertOne(context.TODO(), kehadiran)
+	return err
+}
+
+func GetAllKehadiran(db *mongo.Database) ([]Kehadiran, error) {
+	var kehadiran []Kehadiran
+	cursor, err := db.Collection("kehadiran").Find(context.TODO(), bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	err = cursor.All(context.TODO(), &kehadiran)
+	return kehadiran, err
+}
+
+func UpdateKehadiran(db *mongo.Database, kehadiran Kehadiran) error {
+	_, err := db.Collection("kehadiran").UpdateOne(context.TODO(), bson.M{
+		"date": kehadiran.Date,
+		"name": kehadiran.Name,
+		"subject": kehadiran.Subject,
+	}, bson.M{"$set": kehadiran})
+	return err
+}
+
+func DeleteKehadiran(db *mongo.Database, date string, name string, subject string) error {
+	_, err := db.Collection("kehadiran").DeleteOne(context.TODO(), bson.M{
+		"date": date,
+		"name": name,
+		"subject": subject,
+	})
+	return err
 }
